eb19e73a21e0cd52a5a877d0fd21f0d3fd8bc58d
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-02-25 14:31:41 +0300

    deleting items

--- mini_course_JS_BootStrap/js/main.js
+++ mini_course_JS_BootStrap/js/main.js
@@ -6,6 +6,13 @@ const myModal = new bootstrap.Modal(document.querySelector('#exampleModal'), {
     keyboard: false,
 })
 
+let options = {
+    valueNames: ['name', 'price']
+};
+let userList;
+
+update_goods();
+
 document.querySelector('.add_new').addEventListener('click', function (e) {
     let name = document.querySelector('#good_name').value;
     let price = document.querySelector('#good_price').value;
@@ -29,7 +36,6 @@ document.querySelector('.add_new').addEventListener('click', function (e) {
             })
     }
 })
-update_goods()
 function update_goods() {
     let result_price = 0;
     let tbody = document.querySelector('.list');
@@ -46,8 +52,8 @@ function update_goods() {
 <td class="name">${goods[i][1]}</td>
 <td class="price">${goods[i][2]}</td>
 <td>${goods[i][3]}</td>
-<td><button class="btn good_delete btn-danger" data-delete="${goods}[i][0]">&#10006;</button></td>
-<td><button class="btn good_delete btn-primary" data-goods="${goods}[i][0]">&#10149;</button></td>
+<td><button class="btn good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
+<td><button class="btn good_delete btn-primary" data-goods="${goods[i][0]}">&#10149;</button></td>
 </tr> `
             )
             if (goods[i][4] > 0) {
@@ -59,16 +65,48 @@ function update_goods() {
 <td class="price_name">${goods[i][1]}</td>
 <td class="price_one">${goods[i][2]}</td>
 <td class="price_count">${goods[i][4]}</td>
-<td class="price_discount"><input type="text" data-good-id="${goods}[i][0]" value="goods[i][5]" min="0" max="100"></td>
+<td class="price_discount"><input type="text" data-good-id="${goods[i][0]}" value="${goods[i][5]}" min="0" max="100"></td>
 <td>${goods[i][6]}</td>
-<td><button class="btn good_delete btn-danger" data-delete="${goods}[i][0]">&#10006;</button></td>
+<td><button class="btn good_delete btn-danger" data-delete="${goods[i][0]}">&#10006;</button></td>
 </tr> `)
             }
         }
-        // userList = new List('goods', options);
+        userList = new List('goods', options);
     } else {
         table1.hidden = true;
         table2.hidden = true;
     }
     document.querySelector('.price_result').innerHTML = result_price + ' &#8381;';
 }
+
+document.querySelector('.list').addEventListener('click', function (e) {
+    if (!e.target.dataset.delete) {
+        return
+    }
+    Swal.fire({
+            title: 'Внимание',
+            text: 'Вы действительно хотите удалить?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Да',
+            cancelButtonText: 'Отмена'
+        }).then(result => {
+            console.log(result)
+            if(result.isConfirmed) {
+                let goods = JSON.parse(localStorage.getItem('goods'));
+                console.log(goods);
+                for (let i = 0; i < goods.length; i++) {
+                    console.log(goods[i][0])
+                    if (goods[i][0] == e.target.dataset.delete) {
+                        goods.splice(i, 1);
+                        console.log(goods)
+                        localStorage.setItem('goods', JSON.stringify(goods));
+                        update_goods();
+                    }
+                }
+                Swal.fire('Удалено','Выбранный товар был удалён', 'success')
+            }
+    })
+})
