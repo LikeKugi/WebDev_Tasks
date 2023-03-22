0f17e97084726e54e31f0eeb75a8449e6306631f
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-03-23 00:59:47 +0300

    feat(accordion): add opening and closing

---
+++ SB_Frontend/html_css/5/js/Details.js
@@ -0,0 +1,21 @@
+class Details {
+  constructor(elem = ".accordion") {
+    this.elem = document.querySelector(elem);
+    console.log(this.elem);
+    this.elem.addEventListener("click", (e) => {
+      this.clickAction(e);
+    });
+  }
+
+  clickAction(e) {
+    console.log(e.target);
+    const targetEl = e.target.closest(".accordion__details");
+    console.log(targetEl);
+    let textEl = targetEl.querySelector(".accordion__box-text");
+    console.log(textEl);
+    textEl.classList.toggle("accordion__hidden");
+    textEl.style.height = textEl.scrollHeight + "px";
+    targetEl.querySelector(".accordion__box-title svg").webkitTransform +=
+      "rotateX(45deg)";
+  }
+}
