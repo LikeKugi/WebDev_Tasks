d6ff37f19dd5e062deb65363dba4db64ba25bd58
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-03-25 04:22:42 +0300

    feat(Burger): add Burger menu js on max-width 1200px

---
+++ SB_Frontend/html_css/5/js/Burger.js
@@ -0,0 +1,41 @@
+class Burger {
+  constructor(elem = ".header__nav") {
+    this.container = document.querySelector(elem);
+    this.togglerEls = this.container.querySelectorAll(".nav__toggler");
+    this.togglerEls.forEach((el) =>
+      el.addEventListener("click", () => {
+        this.toggleList();
+      })
+    );
+    this.navList = this.container.querySelector(".nav__list");
+    this.windowsQuery();
+  }
+
+  hideList() {
+    this.navList.classList.add("nav__hidden");
+    this.navList.style.height = 0;
+  }
+  showList() {
+    this.navList.classList.remove("nav__hidden");
+    this.navList.style.height = "100vh";
+  }
+
+  toggleList() {
+    if (this.navList.classList.contains("nav__hidden")) {
+      this.showList();
+    } else {
+      this.hideList();
+    }
+  }
+
+  windowsQuery() {
+    let query = window.matchMedia("(max-width: 1200px)");
+    window.addEventListener("resize", () => {
+      if (query.matches) {
+        this.navList.classList.add("nav__hidden");
+      } else {
+        this.navList.classList.remove("nav__hidden");
+      }
+    });
+  }
+}
